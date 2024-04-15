0c10b5189339765af45520377894a0b977d8d918
vijayshagara <vijayshagara1221@gmail.com>
2024-04-15 17:56:10 +0530

    env added

--- models/index.js
+++ models/index.js
@@ -10,15 +10,11 @@ const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 let sequelize;
-// Check if config is properly loaded
-// if (!config) {
-//   throw new Error(`Unable to load database configuration for environment '${env}'`);
-// }
-
-sequelize = new Sequelize(...config, {
-  host: config.host,
-  dialect: 'postgres'
-});
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
 fs
   .readdirSync(__dirname)
